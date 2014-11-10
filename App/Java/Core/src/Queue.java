dd7fc46a2e1499ed8f641a21d3210bb4024da75c
Yaron Shani <yaron.shani@gmail.com>
2014-11-10 21:49:18 +0200

    added eclipse project with ant support

--- App/Java/Core/src/Queue.java
+++ App/Java/Core/src/Queue.java
@@ -1,10 +1,12 @@
+import com.sun.tools.javac.util.List;
+
 public class Queue {
 	List<SongEntry> song_entries;
 	Integer current_song;
-	public SongEntry GetNext() {}
-	public SongEntry GetPrevious() {}
-	public SongEntry GetSong() {}
-	public SongEntry GetSong(int pos) {}
+	public SongEntry GetNext() {return null;}
+	public SongEntry GetPrevious() {return null;}
+	public SongEntry GetSong() {return null;}
+	public SongEntry GetSong(int pos) {return null;}
 	public void AddSong(SongEntry song) {}
 	public void Remove(Integer pos) {}
 	public void Reorder(Integer a, Integer b) {}
--- App/Java/Core/src/Song.java
+++ App/Java/Core/src/Song.java
@@ -1,6 +1,8 @@
+import com.sun.javafx.collections.MappingChange.Map;
+
 public class Song {
 	String id;
-	Tags tags;
+	Map<String, String> tags;
 	String file_path;
 	Integer file_size;
 	Integer length;
